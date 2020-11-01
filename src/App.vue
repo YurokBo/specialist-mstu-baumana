3b2a5d7ab70b2e1e2daefcc8cc8a93295a43b833
YurokBo <vertel3012@mail.ru>
2020-11-01 23:32:06 +0300

    files added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Content />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import Header from "./components/Header/Header";
+    import Content from "./components/Content/Content";
+
+    export default {
+        name: 'App',
+        components: {
+            Content,
+            Header
+
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "@/assets/styles/base.scss";
 </style>
---
+++ src/components/Content/Form/Form.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div class="form__box">
+        <form class="form">
+
+            <label for="search" class="form__label">Название курса</label>
+            <div class="form__content">
+                <input class="form__input" type="text" id="search" placeholder="Начните поиск...">
+                <button class="form__btn" type="button">Найти курс</button>
+            </div>
+        </form>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Form"
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "Form";
+</style>
